cd00c63f1e20907292dab857e8df08a9859f2743
jsanders36 <jsaf36@gmail.com>
2023-07-18 10:03:28 -0600

    Up to Date

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 //console.log('My code is running');
-let task =document.querySelector("task entered");
-let button=document.querySelector("add task");
+let task =document.querySelector('#task entered');
+let button=document.querySelector('#add task');
+button.addEventListener("click", addTask)
+addTask => {
+  let task =
+}
